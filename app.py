18ecc4128cd62a22a511f40620841391ef1f026a
Chris Schmitt <cjschmitt620@gmail.com>
2022-12-13 00:39:45 -0500

    adding requirements

--- app.py
+++ app.py
@@ -43,15 +43,20 @@ def user_redirect():
 
 @app.route('/user/<connect_code>', methods=['POST','GET'])
 def user(connect_code):
-    connect_code = connect_code.replace("-","#")
+    connect_code = connect_code.replace("-","#").upper()
     player_rating_class = PlayerRatingClass()
     player_rating_class.insert_new_rating(connect_code)
 
     player_ratings = PlayerRating.query.where(PlayerRating.connect_code.ilike(connect_code)).order_by(PlayerRating.datetime).limit(10).all()
     datetimes = []
     ratings = []
-
-    return render_template('user.html', player_ratings=player_ratings, connect_code=connect_code)
+    for rating in player_ratings:
+        datetimes.append(rating.datetime.strftime("%m/%d/%Y, %H:%M:%S"))
+        ratings.append(rating.rating)
+        print(rating.rating)
+    print(datetimes)
+    
+    return render_template('user.html', player_ratings=player_ratings, connect_code=connect_code, datetimes=datetimes, ratings=ratings)
 
 @app.route('/all_ratings', methods=['GET'])
 def all_ratings():
