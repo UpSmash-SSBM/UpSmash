ea11067d52d3f3d7864fb7827f6299cb0ee3fa64
Ryan Yeung <litterjack1@gmail.com>
2023-01-15 17:46:09 -0500

    Desktop App framework. Needs updating

---
+++ UpSmash-app/src/main.js
@@ -0,0 +1,106 @@
+const { app, BrowserWindow, Menu, dialog, ipcMain } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+//if (require('electron-squirrel-startup')) {
+//  app.quit();
+//}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 900,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  //Build Menu from Template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  //Insert Menu
+  Menu.setApplicationMenu(mainMenu);
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow) ;
+
+//Catch file adds
+ipcMain.on('fileList', function(e, item){
+  for (files in item) {
+    const data = item[files].split("\\");
+    const name = data[data.length - 1];
+    console.log(name);
+    fs.copyFile(item[files], 'C:\\Users\\Ryan\\Documents\\UpSmash\\UpSmash\\static\\files\\' + name, (err) =>{
+      if (err) throw err;
+      console.log(item[files] + 'has been copied successfully')
+    })
+  }
+  console.log(item)
+})
+// Create menu template 
+const mainMenuTemplate = [
+  {
+    label:'File',
+    submenu: [
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' :
+        'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+//If Mac add empty object to menu 
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({});
+}
+// Add Dev tools item if not in production
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Dev tools',
+    submenu: [
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' :
+        'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  });
+}
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
