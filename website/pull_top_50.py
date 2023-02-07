b92e3a4d2a16779666f4580eeb823cc642a33f40
Chris Schmitt <cjschmitt620@gmail.com>
2023-02-07 16:19:45 -0500

    adding top_50 player script

--- website/pull_top_50.py
+++ website/pull_top_50.py
@@ -4,29 +4,37 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 import time
+import json
+import os
 
 app = create_full_app()
 
-top_50_players = []
-engine_url = ['sqlite:///db.sqlite3']
-url = "https://slippi.gg/leaderboards?region="
-regions = ['na', 'eu', 'other']
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Firefox(options=options)
-for region in regions:
-    driver.get(url + region)
-    time.sleep(2)
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    rows = soup.find("table").find("tbody").find_all("tr")
-    for row in rows:
-        cells = row.find_all("td")
-        player_name = cells[2].find("a").get_text()
-        player_tag = cells[2].find("p").get_text()
-        top_50_players.append((player_name, player_tag))
-driver.close()
+file_output = 'top_50_players.json'
+if not os.path.exists(file_output):
+    top_50_players = []
+    engine_url = ['sqlite:///db.sqlite3']
+    url = "https://slippi.gg/leaderboards?region="
+    regions = ['na', 'eu', 'other']
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
+    for region in regions:
+        driver.get(url + region)
+        time.sleep(2)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        rows = soup.find("table").find("tbody").find_all("tr")
+        for row in rows:
+            cells = row.find_all("td")
+            player_name = cells[2].find("a").get_text()
+            player_tag = cells[2].find("p").get_text()
+            top_50_players.append((player_name, player_tag))
+    driver.close()
 
-for player in top_50_players:
-   print(player[1])
-   get_or_create_player(player[1])
+    print(json.dumps(top_50_players))
+else:
+    with open(file_output) as player_file:
+        top_50_players = json.load(player_file)
+        for player in top_50_players:
+            print(player[1])
+            get_or_create_player(player[1])
